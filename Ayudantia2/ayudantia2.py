d16e54b4de20797567e0e9301661c21338ce588f
Cristobal Drago <crdrago@alumnos.uai.cl>
2021-03-21 22:47:15 -0300

    Ayudantia 2

---
+++ Ayudantia2/ayudantia2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+# Plotly documentation: https://plotly.com/python/
+import plotly.offline as pyo
+import plotly.graph_objects as go
+
